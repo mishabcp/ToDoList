f21279e4cebd82258f654a09825cf72bebd3cb71
mishabcp <142352354+mishabcp@users.noreply.github.com>
2024-03-16 00:05:55 +0530

    retry

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    reactRefresh(),
-    tailwindcss(),
-    autoprefixer(),
-  ],
-});
+  plugins: [react()],
+  base: "/ToDoList/"
+})

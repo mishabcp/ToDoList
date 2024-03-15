ca59584d3c253fbafd5da38b51708f00ca6e878d
mishabcp <142352354+mishabcp@users.noreply.github.com>
2024-03-16 00:21:24 +0530

    retry

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
 import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
+
 
 export default defineConfig({
   plugins: [
-    reactRefresh(),
     tailwindcss(),
-    
   ],
 });
